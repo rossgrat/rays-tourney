38c4b153ce7adf5d66d43cb2095e1d74cca64fff
Ross Grattafiori <ross@grattafiori.dev>
2024-05-06 21:05:45 -0400

    Genarating rounds where the byes are only off by one game at most is working, one idea for equal byes is semi-working, fails in some cases, and prime numbers produce too long of rounds

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 * Multipeer connectivity and scoring
     * Users connect to a game, similar to kahoot then enter their scores after each game, app automatically keeps track of a leaderboard
     * Scoring algorithm needs to support uneven amount of games played
+
+
+## TODO
+* We need to intelligently decide the number of tables per round based on the number of byes such that
+* the number of byes is equal
--- src/app/tourney-engine-2.ts
+++ src/app/tourney-engine-2.ts
@@ -1,3 +1,5 @@
+import { first } from "rxjs"
+
 interface Table {
     Teams: number[][] // [Team][Player]
 }
@@ -10,17 +12,30 @@ interface Round {
 
 interface Game {
     Rounds: Round[]
+    Byes: number[]
 }
 
+interface GameInputs {
+    NumPlayers: number
+    NumRounds: number
+    NumTables: number
+    Mode: GameMode
+}
+
+enum GameMode {
+    Custom = 1,
+    MaxRounds,
+    EqualByes,
+}
 
 // Attempt to find the first player and partner combination where
 // 1. The partner is not the player
 // 2. The player and partner have not yet been on a team togetther
 // 3. The player and partner are not already on some other team in the round
-function selectTeam(numPlayers: number, usedPartners: Set<number>[], usedPlayers: Set<number>): number[] {
-    for (let player = 0; player < numPlayers; player++) {
-        if ((!usedPlayers.has(player))&& (usedPartners[player].size < numPlayers)) {
-            for (let partner = 0; partner < numPlayers; partner++) {
+function selectTeam(numPlayers: number, orderedPlayers: number[], usedPartners: Set<number>[], usedPlayers: Set<number>): number[] {
+    for (let player of orderedPlayers) {
+        if ((!usedPlayers.has(player)) && (usedPartners[player].size < numPlayers)) {
+            for (let partner of orderedPlayers) {
                 if ((player != partner) && !usedPartners[player].has(partner) && !usedPlayers.has(partner)) {
                     usedPartners[player].add(partner)
                     usedPartners[partner].add(player)
@@ -44,63 +59,129 @@ function getUnusedPlayers(numPlayers: number, usedPlayers: Set<number>): number[
     return arr
 }
 
-export function GenRounds(numPlayers: number): Round[] {
-    if (numPlayers < 4) {
-        return []
+function maxDivisor(end: number, num: number): number {
+    for (let i = end; i >=0; i--) {
+        if (num % i == 0) {
+            return i
+        }
+    }
+    return 1
+}
+
+
+export function GenRounds(gi: GameInputs): Game | undefined {
+    if (gi.NumPlayers < 4) {
+        return undefined
     }
     let usedPartners: Set<number>[] = []
-    for (let i = 0; i < numPlayers; i ++) {
+    for (let i = 0; i < gi.NumPlayers; i ++) {
         usedPartners.push(new Set())
     }
 
-    let rounds: Round[] = []
+    let game: Game = {
+        Rounds: [],
+        Byes: new Array(gi.NumPlayers).fill(0)
+    }
 
-    const playersPerTeam = 2
+    const playersPerTeam = 2 
     const teamsPerTable = 2
-    let numTables: number = Math.floor(numPlayers / (playersPerTeam * teamsPerTable))
 
-    while(true) {
+    switch (gi.Mode) {
+        case GameMode.MaxRounds:
+            gi.NumTables = Math.floor(gi.NumPlayers / (playersPerTeam * teamsPerTable))
+            gi.NumRounds = Number.MAX_SAFE_INTEGER
+            break
+        case GameMode.EqualByes:
+            gi.NumTables = maxDivisor(Math.floor(gi.NumPlayers / (playersPerTeam * teamsPerTable)), gi.NumPlayers)
+            gi.NumRounds = gi.NumPlayers / gi.NumTables
+            break
+    }
+
+
+
+    let playerByesDesc: number[] = []
+    for(let i = 0; i < gi.NumPlayers; i++) {
+        playerByesDesc[i] = i
+    }
+
+    while(game.Rounds.length < gi.NumRounds) {
        let round: Round = {
         Tables: [],
         ByePlayers: [],
         UsedPlayers: new Set()
        }
-        while (round.Tables.length < numTables) {
+        while (round.Tables.length < gi.NumTables) {
             let table: Table = {
                 Teams: []
             }
+
             while(table.Teams.length < teamsPerTable) {
-                let team = selectTeam(numPlayers, usedPartners, round.UsedPlayers)
+                let team: number[] = []
+                team = selectTeam(gi.NumPlayers, playerByesDesc, usedPartners, round.UsedPlayers)
+                team.sort()
                 if (team.length == 0) {
-                    return rounds
+                    if (round.Tables.length > 0) {
+                        round.ByePlayers = getUnusedPlayers(gi.NumPlayers, round.UsedPlayers)
+                        game.Rounds.push(round)
+                    }
+                    if (table.Teams.length > 0) {
+                        for (let player of table.Teams[0]) {
+                            round.UsedPlayers.delete(player)
+                        }
+                    }
+                    return game
                 }
                 table.Teams.push(team)
             }
             round.Tables.push(table)
         }
-        round.ByePlayers = getUnusedPlayers(numPlayers, round.UsedPlayers)
-        rounds.push(round)
-    }
-
-}
-
-
-function countByes(numPlayers: number, rounds: Round[]) {
-    let byes: number[] = new Array(numPlayers).fill(0)
-    
-    for (let round of rounds) {
+        round.ByePlayers = getUnusedPlayers(gi.NumPlayers, round.UsedPlayers)
         for (let player of round.ByePlayers) {
-            byes[player] = byes[player] + 1
+            game.Byes[player] = game.Byes[player] + 1
         }
+
+        playerByesDesc.sort(function(a: number, b: number): number {
+            if (game.Byes[a] < game.Byes[b]) {
+                return 1
+            } else if (game.Byes[a] > game.Byes[b]) {
+                return -1
+            } else {
+                return 0
+            }
+        })
+
+        // console.log("BYES: ",game.Byes)
+        // console.log("ORDER: ",playerByesDesc)
+        // for(let s of usedPartners) {
+        //     console.log(s)
+        // }
+
+        game.Rounds.push(round)
     }
-    console.log("BYES: ",byes)
+    return game
 }
+
 
 export function Run() {
-    for (let i = 4; i < 30; i++) {
+    for (let i = 7; i < 28; i++) {
         console.log("NUM PLAYERS: ", i)
-        let rounds = GenRounds(i)
-        countByes(i, rounds)
-        console.log(rounds)
+        let gi:GameInputs = {
+            NumPlayers: i,
+            NumRounds: -1,
+            NumTables: -1,
+            Mode: GameMode.EqualByes
+        }
+        let game = GenRounds(gi) as Game
+        console.log(game.Rounds)
+        console.log(game.Byes)
     }
 }
+
+
+/*
+    Two Options:
+    1. Pick the number of rounds and the number of tables per round, some players may have more byes than others, automatic scoring system 
+    2. Program optimizes the number of 
+
+
+*/
